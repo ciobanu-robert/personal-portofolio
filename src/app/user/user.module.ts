db142030a595bd92b1c4fed0d8fddef59c9f2173
Robi <ciobanurobert100@gmail.com>
2024-08-08 01:10:31 +0300

    added about me

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { PageComponent } from './page/page.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     PageComponent,
     NavigationBarComponent,
     HomeComponent,
+    AboutComponent,
   ],
   imports: [
     CommonModule,
