da80ee924ccadee5631517af54a278262ad399a1
Robi <ciobanurobert100@gmail.com>
2024-08-26 22:06:21 +0300

    added my projects

--- src/app/user/projects/projects.component.ts
+++ src/app/user/projects/projects.component.ts
@@ -14,42 +14,44 @@ export class ProjectsComponent {
       date: '02/2024 - 05/2024',
       image: '/assets/img/compare-rgm.png',
       description: 
-      'The Compare RGM platform offers companies ' +
-      'the ability quickly and efficiently compare ' +
-      'products and services offered by competitors, ' +
-      'giving them the freedom to make informed ' +
-      'decisions and identify the best options according ' +
-      'to their needs and preferences.',
+        'The Compare RGM platform offers companies ' +
+        'the ability quickly and efficiently compare ' +
+        'products and services offered by competitors, ' +
+        'giving them the freedom to make informed ' +
+        'decisions and identify the best options according ' +
+        'to their needs and preferences.',
       technologies: ['Angular', 'Node.js', 'MongoDB'],
       link: 'https://github.com/ciobanu-robert/CompareRGM'
     },
     {
-      name: 'Compare RGM',
-      date: '02/2024 - 05/2024',
-      image: '/assets/img/compare-rgm.png',
+      name: 'Calendar app',
+      date: '08/2024 - 08/2024',
+      image: '/assets/img/calendar-app.png',
       description: 
-      'The Compare RGM platform offers companies ' +
-      'the ability quickly and efficiently compare ' +
-      'products and services offered by competitors, ' +
-      'giving them the freedom to make informed ' +
-      'decisions and identify the best options according ' +
-      'to their needs and preferences.',
-      technologies: ['Angular', 'Node.js', 'MongoDB'],
-      link: 'https://github.com/ciobanu-robert/CompareRGM'
+        'A sleek and intuitive calendar app crafted for ' +
+        'effortless scheduling, timely reminders, and ' + 
+        'comprehensive event management. Stay organized ' +
+        'with a clear overview of all your important dates, ' +
+        'appointments, and tasks, all accessible in one ' +
+        'convenient place.',
+      technologies: ['Angular'],
+      link: 'https://github.com/ciobanu-robert/calendar-app'
     },
     {
-      name: 'Compare RGM',
-      date: '02/2024 - 05/2024',
-      image: '/assets/img/compare-rgm.png',
+      name: 'Portofolio',
+      date: '08/2024 - Present',
+      image: '/assets/img/portofolio.png',
       description: 
-      'The Compare RGM platform offers companies ' +
-      'the ability quickly and efficiently compare ' +
-      'products and services offered by competitors, ' +
-      'giving them the freedom to make informed ' +
-      'decisions and identify the best options according ' +
-      'to their needs and preferences.',
-      technologies: ['Angular', 'Node.js', 'MongoDB'],
-      link: 'https://github.com/ciobanu-robert/CompareRGM'
+        'This portfolio project is designed to showcase my ' +
+        'skills, experience, and accomplishments as a ' +
+        'developer. It highlights my expertise in various ' +
+        'technologies and tools through a collection of ' +
+        'projects, and code samples. With a clean and ' +
+        'professional design, the portfolio provides a clear ' +
+        'overview of my abilities, including web development, ' +
+        'programming languages, and problem-solving.',
+      technologies: ['Angular'],
+      link: 'https://github.com/ciobanu-robert/portofolio'
     },
   ];
 }
